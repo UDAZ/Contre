42e4973aaa21ef732fa56cb4427e586ff8a63f16
UDAZ <u.ka2.sally.cinnamon@gmail.com>
2021-06-08 16:59:16 +0000

    add def find_for_github_oauth

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,11 @@ class User < ApplicationRecord
   validates :name, presence: true, uniqueness: true
   validates :email, presence: true, uniqueness: true
   validates :uid, presence: true, uniqueness: { scope: :provider }
+  def self.find_for_github_oauth(auth)
+    where(provider: auth.provider, uid: auth.uid).first_or_create! do |user| 
+      user.name = auth.info.name
+      user.email = auth.info.email
+      user.password = Devise.friendly_token[0, 20]
+    end
+  end
 end
