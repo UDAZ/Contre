48696a4b994c26853d484379387b61438a08f5bc
UDAZ <u.ka2.sally.cinnamon@gmail.com>
2021-06-08 17:45:26 +0000

    add contributionscount

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,6 +13,23 @@ class User < ApplicationRecord
       user.email = auth.info.email
       user.password = Devise.friendly_token[0, 20]
       user.github_url = "https://github.com/" + auth.info.name
+      #githubのnameを取得
+      github_name = '"' + auth.info.name + '"'
+      uri = URI.parse("https://api.github.com/graphql")
+      request = Net::HTTP::Post.new(uri)
+      #github apiにtokenで接続
+      request["Authorization"] = ENV['GITHUB_API']
+      #totalContributionsの取得を要請
+      request.body = JSON.dump({"query" => "query {user (login: #{github_name}) {contributionsCollection {contributionCalendar {totalContributions}}}}"})
+      req_options = {
+        use_ssl: uri.scheme == "https"
+      }
+      #requesetに対するresponseを取得
+      response = Net::HTTP.start(uri.hostname, uri.port, req_options) do |http|
+        http.request(request)
+      end
+      #responseのbody(json型)から数字のみ取り出す
+      user.contributions = response.body.gsub(/[^\d]/, "").to_i
     end
   end
 end
