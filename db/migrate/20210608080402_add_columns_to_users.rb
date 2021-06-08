47be1927ef7f02a5829caf34cdacbbdc7a9aeb67
UDAZ <u.ka2.sally.cinnamon@gmail.com>
2021-06-08 08:08:20 +0000

    add columns

---
+++ db/migrate/20210608080402_add_columns_to_users.rb
@@ -0,0 +1,11 @@
+class AddColumnsToUsers < ActiveRecord::Migration[5.2]
+  def change
+    add_column :users, :name, :string
+    add_column :users, :uid, :string
+    add_column :users, :provider, :string
+    add_column :users, :contributions, :string
+    add_column :users, :github_url, :string
+    # uidとproviderにインデックスとユニークを付与
+    add_index :users, [:uid, :provider], unique: true
+  end
+end
