12c9964e50426931169e7d8ea877ff4a119c3feb
UDAZ <u.ka2.sally.cinnamon@gmail.com>
2021-06-08 16:56:57 +0000

    add validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable,
          :omniauthable, omniauth_providers: [:github]
+  validates :name, presence: true, uniqueness: true
+  validates :email, presence: true, uniqueness: true
+  validates :uid, presence: true, uniqueness: { scope: :provider }
 end
