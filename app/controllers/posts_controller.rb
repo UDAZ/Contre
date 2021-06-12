513c5dccfb15b6e4b0519150f7527bd12378c5a4
UDAZ <u.ka2.sally.cinnamon@gmail.com>
2021-06-12 09:30:08 +0000

    add post action

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,22 +1,34 @@
 class PostsController < ApplicationController
   def index
+    @posts = Post.all
   end
 
   def new
+    @post = Post.new
   end
   
   def create
+    @post = Post.new(post_params)
+    @post.user_id = current_user.id
+    @post.save
   end
 
   def edit
+    @post = Post.find(params[:id])
   end
   
   def update
+    @post = Post.find(params[:id])
+    @post.update(post_params)
   end
   
   def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
   end
 
   def show
+    @post = Post.find(params[:id])
+    @user = @post.user
   end
 end
