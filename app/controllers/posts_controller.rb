a23980f72d891eecf9ce9246bc73796b918cd656
UDAZ <u.ka2.sally.cinnamon@gmail.com>
2021-06-12 13:36:24 +0000

    add show edit

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -12,15 +12,18 @@ class PostsController < ApplicationController
     @post = Post.new(post_params)
     @post.user_id = current_user.id
     @post.save
+    redirect_to post_path(@post)
   end
 
   def edit
     @post = Post.find(params[:id])
+    @genres = Genre.all
   end
   
   def update
     @post = Post.find(params[:id])
     @post.update(post_params)
+    redirect_to post_path(@post)
   end
   
   def destroy
