e5699901ea8998212903f37c6e3455c54efbc8b6
UDAZ <u.ka2.sally.cinnamon@gmail.com>
2021-06-16 13:57:32 +0000

    add view

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -1,7 +1,8 @@
 class ReportsController < ApplicationController
   def create
     @post = Post.find(params[:post_id])
-    report = current_user.reports.build(post_id: params[:post_id])
+    report = current_user.reports.build(post_id: params[:post_id], description: params[:description])
     report.save
+    redirect_to post_path(@post)
   end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -6,4 +6,7 @@ class Post < ApplicationRecord
   def favorited_by?(user)
     favorites.where(user_id: user.id).exists?
   end
+  def reported_by?(user)
+    reports.where(user_id: user.id).exists?
+  end
 end
