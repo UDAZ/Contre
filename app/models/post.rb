a5526a96bbd468bfb29da3722ba16e337dfc8ca5
UDAZ <u.ka2.sally.cinnamon@gmail.com>
2021-06-19 05:58:43 +0000

    add def

--- app/models/post.rb
+++ app/models/post.rb
@@ -10,4 +10,21 @@ class Post < ApplicationRecord
   def reported_by?(user)
     reports.where(user_id: user.id).exists?
   end
+  def create_notification_like!(current_user)
+    # すでに「いいね」されているか検索
+    temp = Notification.where(["visitor_id = ? and visited_id = ? and post_id = ? and action = ? ", current_user.id, user_id, id, 'like'])
+    # いいねされていない場合のみ、通知レコードを作成
+    if temp.blank?
+      notification = current_user.active_notifications.new(
+        post_id: id,
+        visited_id: user_id,
+        action: 'like'
+      )
+      # 自分の投稿に対するいいねの場合は、通知済みとする
+      if notification.visitor_id == notification.visited_id
+        notification.checked = false
+      end
+      notification.save if notification.valid?
+    end
+  end
 end
