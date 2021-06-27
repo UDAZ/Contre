c08d9ae61edcdd5324e14370d731cb3285df49db
UDAZ <u.ka2.sally.cinnamon@gmail.com>
2021-06-27 18:55:05 +0000

    add guest sign in

---
+++ app/controllers/users/sessions_controller.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+module Users
+  class SessionsController < Devise::SessionsController
+    def new_guest
+      user = User.guest
+      sign_in user
+      redirect_to root_path, notice: t('header.guestmessage')
+    end
+  end
+end
